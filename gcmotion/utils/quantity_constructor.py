4eaad1688e6db23ab8ebc24642b948367a446a7b
George-Tsiamasiotis <gtsiamasiotis@gmail.com>
2025-03-22 18:55:41 +0200

    enh: added QuantityConstructor()'s arguements as Q's attributes

--- gcmotion/utils/quantity_constructor.py
+++ gcmotion/utils/quantity_constructor.py
@@ -123,4 +123,17 @@ def QuantityConstructor(
     # definition if this function is called many times with the different
     # parameters inside the same session
     set_application_registry(ureg)
-    return ureg.Quantity
+    Q = ureg.Quantity
+
+    # Add arguements as its attributes for easy access
+    args = {
+        "species": species,
+        "B0": B0,
+        "R": R,
+        "a": a,
+        "_psi_wallNU": _psi_wallNU,
+    }
+
+    setattr(Q, "args", args)
+
+    return Q
