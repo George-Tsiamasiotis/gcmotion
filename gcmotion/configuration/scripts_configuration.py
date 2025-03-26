7954c9a7952ccef4f7b5b55237bcaa468ea3e971
George Tzimopoulos <georgetzim23@gmail.com>
2025-03-26 18:52:18 +0200

    fix: removed res range config

--- gcmotion/configuration/scripts_configuration.py
+++ gcmotion/configuration/scripts_configuration.py
@@ -101,3 +101,32 @@ class ContourOrbitConfig:
     copassing_color: str = "xkcd:light purple"
     cupassing_color: str = "xkcd:navy blue"
     undefined_color: str = "xkcd:blue"
+
+
+# -------------- Fixed Points - Bifurcation Config ---------------------------
+
+
+@dataclass()
+class FixedPointsConfig:
+    thetalim: tuple = (-np.pi, np.pi)
+    psilim: tuple = (0, 1.8)
+    fp_method: str = "fsolve"
+    dist_tol: float = 1e-3
+    fp_ic_scan_tol: float = 5 * 1e-8
+    ic_fp_theta_grid_density: int = 500
+    ic_fp_psi_grid_density: int = 101
+    fp_ic_scaling_factor: float = 90
+    fp_random_init_cond: bool = False
+    fp_info: bool = False
+    fp_ic_info: bool = False
+    fp_only_confined: bool = False
+
+
+@dataclass()
+class BifurcationConfig:
+    bif_info: bool = False
+    calc_energies: bool = False
+    energy_units: str = "NUJoule"
+    flux_units: str = "NUmf"
+    energies_info: bool = False
+    which_COM: str = "Pzeta"
